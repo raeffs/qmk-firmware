ec40f4955ca2d56492c857dea3e3f6cd9077f082
Raphael Fleischlin <git@raeffs.dev>
2025-02-01 15:12:10 +0100

    keymap(lulu): querty layout

--- keyboards/boardsource/lulu/rp2040/keymaps/raeffs/rules.mk
+++ keyboards/boardsource/lulu/rp2040/keymaps/raeffs/rules.mk
@@ -1,2 +1,4 @@
+# Enables Link Time Optimization (LTO) when compiling the keyboard.
+# This makes the process take longer, but it can significantly reduce the compiled size (and since the firmware is small, the added time is not noticeable).
 LTO_ENABLE = yes
-AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
