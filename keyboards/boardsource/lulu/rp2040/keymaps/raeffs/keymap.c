f1c20a7e569011feb13b2b4029d20e1252d55c81
Raphael Fleischlin <git@raeffs.dev>
2025-02-23 11:45:57 +0000

    keymap(lulu): update querty layer

--- keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
+++ keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
@@ -2,41 +2,46 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
-    _RAISE,
-    _LOWER,
-    _ADJUST
+    LAYER_QUERTY,
+    LAYER_NAVIGATION,
+    LAYER_SYMBOL,
+    LAYER_SPECIAL
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+/* Layer Switches */
+#define L_RESET TO(LAYER_QUERTY)
+#define L_NAVIG TO(LAYER_NAVIGATION)
+#define L_SYMBO TO(LAYER_SYMBOL)
+#define L_SPECI TO(LAYER_SPECIAL)
 
-#define KC_AUML RALT(KC_A)
-#define KC_OUML RALT(KC_O)
-#define KC_UUML RALT(KC_U)
+/* Custom Keycodes */
+#define C_SHIFT OSM(MOD_LSFT)
+#define C_ALT OSM(MOD_RALT)
+#define C_SUPER OSM(MOD_LGUI)
+#define C_CTRL OSM(MOD_LCTL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QUERTY
 
  ,-----------------------------------------------------.                     ,-----------------------------------------------------.
- |  ESC   |   1    |   2    |   3    |   4    |   5    |                     |   6    |   7    |   8    |   9    |   0    |        |
+ |        |        |        |        |        |        |                     |        |        |        |        |        |        |
  |--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
- |  TAB   |   Q    |   W    |   E    |   R    |   T    |                     |   Y    |   U    |   I    |   O    |   P    |        |
+ |        |   Q    |   W    |   E    |   R    |   T    |                     |   Y    |   U    |   I    |   O    |   P    |        |
  |--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
- | L CTRL |   A    |   S    |   D    |   F    |   G    |--------.   ,--------|   H    |   J    |   K    |   L    |   ;    |        |
+ |        |   A    |   S    |   D    |   F    |   G    |--------.   ,--------|   H    |   J    |   K    |   L    |   ;    |        |
  |--------+--------+--------+--------+--------+--------|        |   |        |--------+--------+--------+--------+--------+--------|
- | L SFT  |   Z    |   X    |   C    |   V    |   B    |--------|   |--------|   N    |   M    |   ,    |   .    |   /    | R SFT  |
+ |        |   Z    |   X    |   C    |   V    |   B    |--------|   |--------|   N    |   M    |   ,    |   .    |   /    |        |
  `-----------------------------------------------------/        /    \        \----------------------------------------------------'
-                         | L ALT  | L GUI  | LOWER  | / SPACE  /      \ ENTER  \ | RAISE  | BackSp | R GUI  |
+                         |  SUPER |  CTRL  | SPACE  | / SHIFT  /      \ ENTER  \ | RAISE  |   BS   |  ALT   |
                          `------------------------------------'        '------------------------------------'
 */
-[_QWERTY] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,      KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
+[LAYER_QUERTY] = LAYOUT(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+  XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
+  XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+                             C_SUPER, C_CTRL, KC_SPC,  C_SHIFT,     KC_ENT,  L_NAVIG, KC_BSPC, C_ALT
 ),
 
 /* NAVIGATION
@@ -53,12 +58,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          |        |        |        | /        /      \        \ |        |        |        |
                          `------------------------------------'        '------------------------------------'
 */
-[_LOWER] = LAYOUT(
+[LAYER_NAVIGATION] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  XXXXXXX,                       XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX,
   _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                       XXXXXXX, KC_4,    KC_5,    KC_6,    KC_0,    XXXXXXX,
   _______, XXXXXXX, KC_PGUP, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX, _______,
-                             _______, _______, _______, _______,     _______, _______, _______, _______
+                             _______, _______, L_RESET, _______,     _______, L_SYMBO, _______, _______
 ),
 
 /* SYMBOL
@@ -75,12 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          |        |        |        | /        /      \        \ |        |        |        |
                          `------------------------------------'        '------------------------------------'
 */
-[_RAISE] = LAYOUT(
+[LAYER_SYMBOL] = LAYOUT(
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, S(KC_2), S(KC_MINS),KC_LBRC,KC_RBRC,S(KC_6),                       S(KC_1), S(KC_COMM),S(KC_DOT),KC_EQL,S(KC_7),XXXXXXX,
   _______, KC_BSLS, KC_SLSH, S(KC_LBRC),S(KC_RBRC),S(KC_8),                   S(KC_SLSH),S(KC_9),S(KC_0),KC_MINS,S(KC_SCLN),XXXXXXX,
   _______, S(KC_3), S(KC_4), S(KC_BSLS),S(KC_GRV),KC_GRV, XXXXXXX,   XXXXXXX, S(KC_EQL),S(KC_5),S(KC_QUOT),KC_QUOT,KC_SCLN,_______,
-                             _______, _______, _______, _______,     _______, _______, _______, _______
+                             _______, _______, L_RESET, _______,         _______, L_SPECI, _______, _______
 ),
 
 /* SPECIAL
@@ -97,27 +102,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          |        |        |        | /        /      \        \ |        |        |        |
                          `------------------------------------'        '------------------------------------'
 */
-[_ADJUST] = LAYOUT(
+[LAYER_SPECIAL] = LAYOUT(
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______,     _______, _______, _______, _______
+                             _______, _______, L_RESET, _______,     _______, _______, _______, _______
 )
 
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
 #if defined(ENCODER_MAP_ENABLE)
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QWERTY] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_LOWER] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_RAISE] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_ADJUST] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LAYER_QUERTY] = { ENCODER_CCW_CW(QK_MOUSE_WHEEL_LEFT, QK_MOUSE_WHEEL_RIGHT), ENCODER_CCW_CW(QK_MOUSE_WHEEL_DOWN, QK_MOUSE_WHEEL_UP) },
+    [LAYER_NAVIGATION] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LAYER_SYMBOL] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [LAYER_SPECIAL] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 
 #endif // ENCODER_MAP_ENABLE
