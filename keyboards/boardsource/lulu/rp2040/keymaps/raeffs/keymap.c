7668abd638f2a698a993276fb79c2995a07c2cee
Raphael Fleischlin <git@raeffs.dev>
2025-03-08 17:47:15 +0100

    temp

--- keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
+++ keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
   XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
-  XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+  C_ALT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
                              C_SUPER, C_CTRL, KC_SPC,  C_SHIFT,     KC_ENT,  L_NAVIG, KC_BSPC, C_ALT
 ),
 
